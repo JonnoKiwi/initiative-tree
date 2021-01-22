8e5bc303381c755c0638da478f9f2e7d6a898f4b
Jonno <jonno.arcus@delvefore.com>
2021-01-22 09:21:37 -1000

    fix: resolve an error when environment.test.ts runs

--- app/services/reactotron/reactotron.ts
+++ app/services/reactotron/reactotron.ts
@@ -1,5 +1,5 @@
 import Tron from 'reactotron-react-native'
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Immutable from 'seamless-immutable'
 import { reactotronRedux as reduxPlugin } from 'reactotron-redux'
 import sagaPlugin from 'reactotron-redux-saga'
